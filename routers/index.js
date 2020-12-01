e9553f91d37835c5898a5dcfa651e8eeced1afe0
adward <adward0511@gmail.com>
2020-12-01 23:28:43 +0700

    Add api manual insert to postgres heroku

--- routers/index.js
+++ routers/index.js
@@ -1,4 +1,14 @@
 const router = require("express").Router();
 
 router.use("/v1", require("./v1"));
+router.get("/seed", async (req, res) => {
+  // require('../models').Post.destroy({})
+  await require("../models").Post.bulkCreate(
+    require("../mocks/mock-data-posts.json")
+  );
+  await require("../models").User.bulkCreate(
+    require("../mocks/mock-data-users.json")
+  );
+  res.json({ message: true });
+});
 module.exports = router;
